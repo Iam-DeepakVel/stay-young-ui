6c1e9dde460fabeb76fe0d4a17478215e9e0a0b6
iam-deepakvel <deepakvel82@gmail.com>
2023-06-20 20:52:52 +0530

    feat: HomePage api integration in Coupon UI

--- components/Coupon.tsx
+++ components/Coupon.tsx
@@ -1,38 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
-
-const coupons = [
-  {
-    _id: "1",
-    name: "50% discount coupon",
-    image: "/assets/images/coupon-1.jpg",
-  },
-  {
-    _id: "2",
-    name: "100% discount coupon",
-    image: "/assets/images/coupon-2.jpg",
-  },
-  {
-    _id: "3",
-    name: "100% discount coupon",
-    image: "/assets/images/coupon-2.jpg",
-  },
-  {
-    _id: "4",
-    name: "100% discount coupon",
-    image: "/assets/images/coupon-1.jpg",
-  },
-];
+import { CouponDto } from "@/admin/containers/coupon/AllCoupons";
 
 const Coupon = () => {
+  const [coupons, setCoupons] = useState<CouponDto[] | null>(null);
+
+  // Fetching Coupons
+  useEffect(() => {
+    async function fetchCoupons() {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_STAY_YOUNG_API}/coupon`
+      );
+      const coupons = await res.json();
+      setCoupons(coupons);
+    }
+    fetchCoupons();
+  }, []);
   return (
     <>
       {/* Desktop & Tablet */}
       <div className="hidden md:block max-w-7xl w-full mx-auto mb-14">
         <div className="flex items-center justify-center w-full flex-wrap">
-          {coupons.map((coupon) => (
+          {coupons?.map((coupon) => (
             <Image
               width={500}
               height={500}
@@ -54,7 +45,7 @@ const Coupon = () => {
           showArrows={false}
           interval={2500}
         >
-          {coupons.map((coupon) => (
+          {coupons?.map((coupon) => (
             <Image
               key={coupon._id}
               width={500}
