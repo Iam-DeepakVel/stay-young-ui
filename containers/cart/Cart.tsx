3561677b827fdbd848777a6a65b182b6ed510034
iam-deepakvel <deepakvel82@gmail.com>
2023-06-23 16:58:36 +0530

    feat: add coupon UI & check coupon validity api integration

--- containers/cart/Cart.tsx
+++ containers/cart/Cart.tsx
@@ -3,13 +3,84 @@ import Wrapper from "@/layouts/Wrapper";
 import { StoreContext } from "@/store/store";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useContext } from "react";
+import React, { ChangeEvent, useContext, useState } from "react";
+import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CouponDto } from "@/admin/containers/coupon/AllCoupons";
+import { calculatePrice } from "@/utils/utils";
+
+const couponFormSchema = z.object({
+  code: z.string().min(1, "Please enter the coupon code to apply."),
+});
+
+type CouponFormSchemaType = z.infer<typeof couponFormSchema>;
+
+enum discountType {
+  PERCENTAGE = "percentage",
+  PRICE = "price",
+}
 
 const Cart = () => {
   const { state, dispatch } = useContext(StoreContext);
   const {
     cart: { cartItems },
   } = state;
+
+  const [coupon, setCoupon] = useState<CouponDto | null>(null);
+
+  const [message, setMessage] = useState<string>("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<CouponFormSchemaType>({
+    resolver: zodResolver(couponFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<CouponFormSchemaType> = async (data) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_STAY_YOUNG_API}/coupon/check-validity`,
+
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        const coupon = await response.json();
+        setCoupon(coupon);
+
+        coupon.discountType === discountType.PERCENTAGE &&
+          setMessage(`Hurray! You have got a discount of ${coupon.discount}%`);
+
+        coupon.discountType === discountType.PRICE &&
+          setMessage(
+            `Hurray! You have got a flat discount of Rs.${coupon.discount}`
+          );
+      } else {
+        const errorData = await response.json();
+        setCoupon(null);
+        setMessage(errorData.message);
+      }
+    } catch (error) {
+      setCoupon(null);
+      setMessage("Something went wrong. Please try again later.");
+    }
+  };
+
+  const price = calculatePrice(cartItems, coupon);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.value) {
+      setMessage("");
+    }
+  };
+
   return (
     <div className=" w-full md:py-20">
       <Wrapper>
@@ -62,28 +133,84 @@ const Cart = () => {
             <h3 className=" text-lg font-bold">Summary</h3>
             <div className=" p-5 my-5 bg-black/[0.05] rounded-xl">
               <div className=" flex justify-between">
-                <h2 className=" uppercase text-md md:text-lg font-medium text-black">
+                <h2 className="text-md md:text-lg font-medium text-black">
                   Subtotal
                 </h2>
-                <p className=" text-md md:text-lg font-medium text-black">
-                  ₹{" "}
-                  {cartItems.reduce(
-                    (a, c) => a + c.quantity * c.discountedPrice,
-                    0
-                  )}
+                <p className="text-md font-medium text-black">
+                  ₹ {price.subTotal}
                 </p>
               </div>
-              <p className=" text-sm md:text-md py-5 border-t mt-5">
+              <div className=" flex justify-between my-2">
+                <h2 className="text-md md:text-lg font-medium text-black">
+                  Discount
+                </h2>
+                <p className=" text-md font-medium text-black">
+                  ₹ {price.discount || 0}
+                </p>
+              </div>
+              <div className=" flex justify-between border-t pt-2">
+                <h2 className="text-md md:text-xl text-black">Total</h2>
+                <p className=" text-md md:text-xl font-medium text-black">
+                  ₹ {price.total || price.subTotal}
+                </p>
+              </div>
+
+              <p className=" text-sm md:text-md py-5 mt-2">
                 The subtotal reflects the total price of your order , including
                 duties and taxes, before any applicable discounts. It does not
                 include delivery costs.
               </p>
             </div>
 
+            {/* Coupon */}
+            <h3 className=" text-lg font-bold">Coupon Code</h3>
+            <p className="text-sm my-2">
+              Enter a valid coupon code to apply a discount to your order.
+            </p>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex items-center gap-2 my-5"
+            >
+              <div>
+                <input
+                  type="text"
+                  className={` ${
+                    coupon && message
+                      ? "border-green-500 focus:ring-green-500 focus:border-green-500"
+                      : "border-gray-300 focus:ring-black focus:border-black"
+                  } bg-gray-50 border text-gray-900 sm:text-sm rounded-lg p-2.5 `}
+                  placeholder="Enter Coupon Code"
+                  {...register("code")}
+                  onChange={handleInputChange}
+                />
+                {errors.code && (
+                  <p className="text-sm text-red-500 absolute">
+                    {errors.code.message}
+                  </p>
+                )}
+
+                {!errors.code && message && (
+                  <p
+                    className={`text-sm absolute mt-1 ${
+                      coupon ? "text-green-500" : "text-red-500"
+                    }`}
+                  >
+                    {message}
+                  </p>
+                )}
+              </div>
+              <button
+                disabled={isSubmitting}
+                className="w-28 h-10 rounded-lg border border-black text-md transition-transform active:scale-95 font-bold uppercase gap-2 hover:opacity-75 disabled:opacity-40"
+              >
+                Apply
+              </button>
+            </form>
+
             {/* Checkout Button */}
             <a
               href="https://wa.me/+919734567890"
-              className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+              className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mt-10 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
             >
               Checkout
             </a>
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,5 @@
+import { CouponDto } from "@/admin/containers/coupon/AllCoupons";
+
 export function calculateDiscountPercentage(
   actualPrice: number,
   discountedPrice: number
@@ -6,6 +8,27 @@ export function calculateDiscountPercentage(
     ((actualPrice - discountedPrice) / actualPrice) * 100;
   return Math.round(discountPercentage);
 }
+
+export const calculatePrice = (cartItems: any, coupon: CouponDto | null) => {
+  const subTotal = cartItems.reduce(
+    (a: any, c: any) => a + c.quantity * c.discountedPrice,
+    0
+  );
+
+  if (coupon?.discountType === "percentage") {
+    const total = subTotal - subTotal * (coupon.discount * 0.01);
+    const discount = subTotal * (coupon.discount * 0.01);
+    return { total, discount, subTotal };
+  }
+
+  if (coupon?.discountType === "price") {
+    const total = subTotal - coupon.discount;
+    const discount = coupon.discount;
+    return { total, discount, subTotal };
+  }
+
+  return { subTotal };
+};
 
 // Scroll to bottom of the current page
 export const scrollToBottom = () => {
